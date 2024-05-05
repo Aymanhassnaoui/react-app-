58648b98d8606a6311667945bb1d5177f8f529d0
Hassnaoui Ayman <a.hassnaoui@epastelsoft.com>
2024-05-05 21:31:10 +0200

    slider

--- src/compenent/BarMenu.js
+++ src/compenent/BarMenu.js
@@ -1,44 +1,61 @@
-import * as React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Unstable_Grid2';
 import Paper from '@mui/material/Paper';
-
-
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import { afficherMenu } from '../api/apiProduct';
+import { Pagination } from 'swiper/modules';
+
+
 function BarMenu() {
+  const [menu, setMenu] = useState([]);
 
-    const [menu, setMenu] = React.useState([]);
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+ 
+  }));
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-      }));
-  
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const menuData = await afficherMenu();
+      setMenu(menuData);
+    };
 
-    React.useEffect(() => {
-      const fetchMenu= async () => {
-        const menu = await afficherMenu();
-        setMenu(menu);
-      };
-  
-      fetchMenu();
-    }, []);
+    fetchMenu();
+  }, []);
 
-    
   return (
-    <Grid    style={{ display: 'block' ,  }}  container rowSpacing={1} columnSpacing={{ xs: 0, sm: 0, md: 3 }}>
-   {menu.map((menuItem, index) => (
-        <Grid   style={{ display: 'block' ,  width : 'auto'}}key={index} item xs={12} sm={6} md={3}>
-          <Item> <img alt={menuItem.thumbnailUrl} width="100%" src={menuItem.url} /></Item>
-        </Grid>
-      ))}
-  </Grid>
-  )
+    <>
+     <Swiper
+        direction={'vertical'}
+        pagination={{
+          clickable: true,
+        }}
+
+        slidesPerView={3}
+      
+        centeredSlides={true}
+        initialSlide={2}
+        modules={[Pagination]}
+        className="mySwiper"
+        style={{      width: '197px' , borderRadius: '13px', height: '721px', background: 'rgb(35,77,78) linear-gradient(0deg, rgba(35,77,78,1) 14%, rgba(59,59,62,1) 45%, rgba(0,0,0,1) 97%)' }}
+      >
+        {menu.map((menuItem, index) => (
+          <SwiperSlide key={index} >
+            <Item>
+              <img alt={menuItem.thumbnailUrl} width="100%" src={menuItem.url} />
+            </Item>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+
+  );
 }
 
-export default BarMenu
+export default BarMenu;
